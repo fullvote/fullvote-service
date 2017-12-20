b93a2a86604a4e5c289f7a21b48c71be09bc9384
Craig Martin <craig@kujoyardwear.com>
2017-12-19 19:42:56 -0500

    use real code in handler

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-exports.handler = (event, context, callback) => {
-  var responseBody = {
-    key6: 'value3',
-    key5: 'value2',
-    key4: 'value1'
-  };
+const { getWinner } = require('fullvote');
 
-  var response = {
-    statusCode: 200,
-    headers: {
-      my_header: 'my_value'
-    },
-    body: JSON.stringify(responseBody),
-    isBase64Encoded: false
-  };
-  callback(null, response);
+exports.handler = (event, context, callback) => {
+  const ballots = event && event.body && event.body.ballots;
+  if (!ballots) {
+    callback(null, {
+      statusCode: 400,
+      headers: {},
+      body: JSON.stringify({
+        error: 'No ballots provided'
+      }),
+      isBase64Encoded: false
+    });
+  } else {
+    const winnerObject = getWinner(event.body.ballots);
+    callback(null, {
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(winnerObject),
+      isBase64Encoded: false
+    });
+  }
 };
